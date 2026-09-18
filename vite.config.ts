fd3209e806a08f94fcb5207cb0c9d0113716d4d4
FARO1993 <facundo.m.rodriguez1993@gmail.com>
2026-09-18 10:30:37 -0300

    fix export vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
